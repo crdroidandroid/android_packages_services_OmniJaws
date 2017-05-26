93b91137cd0b046e587367bc762befcd6ab7f605
Pranav Vashi <neobuddy89@gmail.com>
2017-05-26 21:09:15 +0530

    Switch to crDroid OWM key -- Read commit message
    Openweathermap provides access to weather only and only if the
    requester uses a registered API key and obeys the following rules:
    (https://openweathermap.org/appid#get)
    - Do not send requests more than 1 time per 10 minutes from one
      device/one API key. Normally the weather is not changing
      so frequently.
    - Free and Startup accounts have limitation of capacity and data
      availability. If you do not get respond from server do not try
      to repeat your request immediately, but only after 10 min.
    
    So, dont pick this commit PLEASE. Instead, visit the OWM site using
    above link to get yourself a personal API key.
    
    Using ours will surely block OWM service for the rest of the users.
    So, BE CONSIDERATE!
    
    Thank you
    crDroid Android Team
    
    Change-Id: I179206d731af0ea4783d223a8d24a00010d9da8d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/omnirom/omnijaws/OpenWeatherMapProvider.java
+++ src/org/omnirom/omnijaws/OpenWeatherMapProvider.java
@@ -38,7 +38,6 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     private static final int FORECAST_DAYS = 5;
     private static final String SELECTION_LOCATION = "lat=%f&lon=%f";
     private static final String SELECTION_ID = "id=%s";
-    private static final String API_KEY = "9f65b19b7a6648346dda93c6973a682c";
 
     private static final String URL_LOCATION =
             "http://api.openweathermap.org/data/2.5/find?q=%s&mode=json&lang=%s&appid=%s";
@@ -328,7 +327,7 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     }
 
     private String getAPIKey() {
-        return mContext.getResources().getString(R.string.owm_api_key, API_KEY);
+        return mContext.getResources().getString(R.string.owm_api_key);
     }
 
     public boolean shouldRetry() {
