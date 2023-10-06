2dff43d8c9167043ba26bffe47769a4519e51f98
Pranav Vashi <neobuddy89@gmail.com>
2023-10-06 08:10:13 +0530

    OmniJaws: Update for latest A13
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- app/src/main/java/org/omnirom/omnijaws/SettingsActivity.java
+++ app/src/main/java/org/omnirom/omnijaws/SettingsActivity.java
@@ -20,7 +20,7 @@ package org.omnirom.omnijaws;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class SettingsActivity extends CollapsingToolbarBaseActivity {
 
